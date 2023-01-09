89d90d094f05bc2f45485030ff82757ee8abb79c
Christian <s194255@student.dtu.dk>
2023-01-09 13:50:36 +0100

    Revert "Update test_training.py"

--- tests/test_training.py
+++ tests/test_training.py
@@ -2,8 +2,6 @@ from src.models.model import Classifier
 import torch
 import pytest
 
-
-
 def can_calculate_loss(model, criterion, batch_size):
     x = torch.randn((batch_size, 1, 28, 28), dtype=torch.float32)
     labels = torch.randint(10, (batch_size,), dtype=torch.int64)
